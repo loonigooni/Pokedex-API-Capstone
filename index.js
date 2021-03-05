23ad77bd6814f091b427e9550ef480e0d9ad0639
Hargun Singh <74521016+loonigooni@users.noreply.github.com>
2021-03-04 19:21:30 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-/*
-function formatQueryParams(params) {
-    const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)} = ${encodeURIComponent(params[key])}`)
-    return queryItems.join('&');
-}
-*/
+let pokedex = document.getElementById('pokedex');
 
-function getPokemon(pokemonName) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
-      .then(response => response.json())
-      .then(responseJson => 
-        displayResults(responseJson))
-      .catch(error => alert('Something went wrong :('));
-      console.log(pokemonName);
-}
-
-function displayResults(responseJson) {
-  console.log(responseJson);
-  let results = [];
-//name, weight, height, pkmn img
-  results.push(`
-    <h3>${responseJson.name.toUpperCase()}</h3>
-      <p class="poke-height">Height: ${responseJson.height / 10}m</p>
-      <p class="poke-weight">Weight: ${responseJson.weight /10}kg</p>
-    <img src="${responseJson.sprites.front_default}" class="poke-img" >
-    `)
-//abilities
-  results.push(`<h4 class="abilities">Abilities</h4>`)
-  for (let i = 0; i < responseJson.abilities.length; i++) {
-    results.push(`
-      <p class="poke-abilities">${responseJson.abilities[i].ability.name}</p>`)
+function fetchPokemon() {   
+ 
+  let promises = [];
+  for (let i = 1; i <= 898; i++) {   
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url)
+    .then((res) => res.json()));
   }
-//type
-  results.push(`<h4 class="type">TYPE </h4>`)
-  for (let i = 0; i < responseJson.types.length; i++) {
-    results.push(`<h5 class="poke-type"> ${responseJson.types[i].type.name.toUpperCase()}</h5>`)
-  }
-  $('.results').html(results);
-  $('.section').removeClass('hidden');
-}
-function watchForm() {
-    $('#js-form').submit(event => {
-      event.preventDefault();
-      let pokemonName = $('input[type="text"]').val()
-      getPokemon(pokemonName);
+  Promise.all(promises)
+    .then( results => {
+    let pokemon = results.map((data) => ({
+        name: data.name,
+        id: data.id,
+        image: data.sprites['front_default'],
+        type: data.types.map(type => type.type.name).join(", ")
+      }));
+      displayPokemon(pokemon);
     });
-}
+};
 
-$(watchForm);
+function displayPokemon(pokemon) {
+  console.log(pokemon);
+  let pokeCard = pokemon.map( pokemen => `
+    <li class="card">
+      <img class="card-img" src="${pokemen.image}"/>
+      <h2 class="card-title">${pokemen.id}. ${pokemen.name}</h2>
+      <p class="card-subtitle">Type: ${pokemen.type}</p>
+    </li>
+  `).join('');
+  pokedex.innerHTML = pokeCard;
+}
+ 
+fetchPokemon();
